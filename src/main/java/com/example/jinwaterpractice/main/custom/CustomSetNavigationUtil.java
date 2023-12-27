4b6d2664891a4205740cc88a69b3ded5833b5096
bizplus033 <bizplus033@naver.com>
2023-12-27 13:27:24 +0900

    CustomSetNavigationUtil 디버깅 불변 리스트를 가변 리스트로 변경해서 리턴

--- src/main/java/com/example/jinwaterpractice/main/custom/CustomSetNavigationUtil.java
+++ src/main/java/com/example/jinwaterpractice/main/custom/CustomSetNavigationUtil.java
@@ -1,11 +1,13 @@
 package com.example.jinwaterpractice.main.custom;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public abstract class CustomSetNavigationUtil {
 
     public static List<String> setNavigation(String... items) {
-        return Arrays.asList(items);
+        //return Arrays.asList(items); 이건 불변 리스트를 만들어준다
+         return new ArrayList<>(Arrays.asList(items));
     }
 }
