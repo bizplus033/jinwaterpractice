04de4d05263cf73b219a976ae4c12c35bd89955f
bizplus033 <bizplus033@naver.com>
2023-12-27 17:07:30 +0900

    company 도메인 - Company 엔티티

---
+++ src/main/java/com/example/jinwaterpractice/company/Company.java
@@ -0,0 +1,60 @@
+package com.example.jinwaterpractice.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "company")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String ceo;
+
+    @Column(name = "business_number")
+    private String businessNumber;
+
+    @Column(name = "business_type")
+    private String businessType;
+
+    @Column(name = "business_category")
+    private String businessCategory;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    private String tel;
+
+    private String fax;
+
+    private String email;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
